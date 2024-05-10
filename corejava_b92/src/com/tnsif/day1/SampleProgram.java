688a6ab3c5ad8bbf033761c8f67a7c492777f892
tejas <tejas@DESKTOP-Q9IM2N1>
2024-05-10 17:51:27 +0530

    commit 2

--- corejava_b92/src/com/tnsif/day1/SampleProgram.java
+++ corejava_b92/src/com/tnsif/day1/SampleProgram.java
@@ -2,10 +2,12 @@ package com.tnsif.day1;
 
 public class SampleProgram {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World!!");
+		System.out.println("New change");
 	}
 
 }
